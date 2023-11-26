d7b81511251cf73360cd3d9bd96be3e1091072c4
alifdwt <aputradewantara@outlook.com>
2023-11-26 20:40:12 +0700

    Commit test

--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -4,9 +4,9 @@
 @section('content')
     <div>
         <h1 class="h3 mb-2 text-gray-800">Questions and Answers</h1>
-        <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+        {{-- <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
             For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official
-                DataTables documentation</a>.</p>
+                DataTables documentation</a>.</p> --}}
 
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
